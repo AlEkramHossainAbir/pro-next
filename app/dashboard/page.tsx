a65895673a98511d9650ca6eeaa6c692693e9135
Al- Ekram Hossain Abir <alabir65@gmail.com>
2025-07-16 12:30:12 +0600

    routes

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex align-center justify-center min-h-screen bg-red-100 p-8">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+    </div>
+  );
+}
